4eef3fefcc84c1ca5dbe47365e3328ec75a38131
mitrya <cnexus05@gmail.com>
2024-04-30 19:25:00 +0530

    create middleware for error

--- api/controllers/auth.controller.js
+++ api/controllers/auth.controller.js
@@ -1,7 +1,7 @@
 import User from '../models/user.model.js'
 import bcryptjs from 'bcryptjs'
 
-export const signup = async (req,res) =>{
+export const signup = async (req,res,next) =>{
     
     const {username,email,password} = req.body;
     const hashedPassword = bcryptjs.hashSync(password,10);
@@ -13,6 +13,6 @@ export const signup = async (req,res) =>{
     }
     catch(error)
     {
-        res.status(500).json(error);
+        next(error)
     }
 };
