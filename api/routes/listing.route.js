7e74bd9b1bc265bf35947eb61bcd50b4da5b66b9
mitrya <cnexus05@gmail.com>
2024-05-02 04:45:08 +0530

    create update listing api route

--- api/routes/listing.route.js
+++ api/routes/listing.route.js
@@ -1,5 +1,6 @@
 import express from 'express';
-import { createListing, deleteListing } from '../controllers/listing.controller.js';
+import { createListing, deleteListing, updateListing } from '../controllers/listing.controller.js';
+
 
 
 import { verifyToken } from '../utils/verifyUser.js';
@@ -8,5 +9,6 @@ const router = express.Router();
 
 router.post('/create', verifyToken, createListing);
 router.delete('/delete/:id', verifyToken, deleteListing);
+router.post('/update/:id', verifyToken, updateListing);
 
 export default router;
