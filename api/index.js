0098beb72979ab6eefeb8f3e8fa3a8eb43a9e60b
mitrya <cnexus05@gmail.com>
2024-05-02 05:38:13 +0530

    add pre installation

--- api/index.js
+++ api/index.js
@@ -8,6 +8,7 @@ import listingRouter from './routes/listing.route.js';
 
 import cookieParser from 'cookie-parser'
 
+import path from 'path';
 
 dotenv.config();
 const run = async () => {
@@ -24,6 +25,8 @@ const run = async () => {
 //     console.log(err)
 // })
 
+const __dirname = path.resolve();
+
 const app = express();
 
 app.use(express.json());
@@ -38,6 +41,12 @@ app.listen(3000,()=>{
 app.use("/api/user",userRouter);
 app.use("/api/auth",authRouter);
 app.use('/api/listing', listingRouter);
+
+app.use(express.static(path.join(__dirname, '/client/dist')));
+
+app.get('*', (req, res) => {
+  res.sendFile(path.join(__dirname, 'client', 'dist', 'index.html'));
+})
 
 app.use((err,req,res,next)=>{
     const statusCode = err.statusCode || 500;
